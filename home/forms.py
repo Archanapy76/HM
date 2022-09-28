13526f9570a23010fbe2a92c809b68ced26c7660
unknown <archanaind76@gmail.com>
2022-09-28 11:33:00 +0530

    initial commit

---
+++ home/forms.py
@@ -0,0 +1,21 @@
+from datetime import date
+from pyexpat import model
+from django import forms
+
+
+from .models import Booking
+class DateInput(forms.DateInput):
+        input_type:'date'
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model=Booking
+        fields="__all__"
+
+        widegets = {
+                'booking_date':DateInput()
+        }
+        labels={    'p_name':"Patient Name:",
+                    'p_phone':"Patient Phone:",
+                    'p_email':"Patient Email:",
+                    'doc_name':"Doctor Name:",
+                    'booking_date':"Booking Date:",}      
